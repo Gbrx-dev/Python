71a6630d8475d1fd80ec7774ff0f08af042ef5e5
Gabriel Santos <gabrielhenriquex1566@gmail.com>
2025-05-24 12:01:18 -0300

    Cadastro de produtos

---
+++ Projeto Mão na Massa/index.py
@@ -0,0 +1,8 @@
+from classes.Produto import Produto
+from classes.Categoria import Categoria
+
+produto = Produto('001', 'Celular', 150, 2000)
+produto.listarTodos()
+
+""" categoria = Categoria('Roupas')
+categoria.inserir() """
