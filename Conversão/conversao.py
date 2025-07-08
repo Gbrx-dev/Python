c1443733a61a308cf3e836fbf61c926f69c3c93b
Gabriel Santos <gabrielhenriquex1566@gmail.com>
2025-07-08 11:19:55 -0300

    Adicionado opções de interatividade

--- Conversão/conversao.py
+++ Conversão/conversao.py
@@ -15,8 +15,41 @@ def get_cotacao(destino):
         print('Erro ao obter cotações: ', response.status_code)
         return None
     
-def converterCotaca(origem, destino, valor = 1):
+def converter_cotaca(origem = 'USD', destino = 'BRL', valor = 1):
     rates = get_cotacao(destino)
     return round(valor / rates[origem], 4)
 
-print(converterCotaca('JPY', 'BRL', 2000000))
+def menu():
+    print()
+    print('1- Converter Dólar em Real')
+    print('2- Converter Euro em Real')
+    print('3- Converter Iene em Real')
+    print('4- Outra cotação')
+    print('0- Sair')
+    print()
+
+opcao = 1
+
+
+while opcao != 0:
+    menu()
+    opcao = int(input('Escolha uma opção: '))
+
+    destino = 'BRL'
+    valor = int(input('Digite o valor: '))
+
+    match opcao:
+        case 1: origem = 'USD'
+        case 2: origem = 'EUR'
+        case 3: origem = 'JPY'
+        case 4:
+            origem = input("Digite a ORIGEM: ")
+            destino = input("Digite o DESTINO: ")
+
+    if opcao:
+        print()
+        print('****************************************')
+        print(f'{origem} para {destino}: ', converter_cotaca(origem, destino, valor))
+        print('****************************************') 
+        print()
+                
