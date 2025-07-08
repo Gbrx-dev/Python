77435bcdf7d57434459e7fb47467121a4238b0a4
Gabriel Santos <gabrielhenriquex1566@gmail.com>
2025-07-08 13:39:29 -0300

    grafico da conversao de moedas

--- Conversão/conversao.py
+++ Conversão/conversao.py
@@ -1,23 +1,4 @@
-import requests
-
-def get_cotacao(destino):
-
-    url = 'https://api.exchangerate-api.com/v4/latest/' + destino
-
-    response = requests.get(url)
-    data = response.json()
-
-    if response.status_code == 200:
-
-        return data["rates"]
-
-    else:
-        print('Erro ao obter cotações: ', response.status_code)
-        return None
-    
-def converter_cotaca(origem = 'USD', destino = 'BRL', valor = 1):
-    rates = get_cotacao(destino)
-    return round(valor / rates[origem], 4)
+from moedas import converter_cotaca
 
 def menu():
     print()
